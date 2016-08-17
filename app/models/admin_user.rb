eb3f1a8dbef84808bf4ecff73d12de94c7aec6b5
Jesse B. Hannah <jesse@jbhannah.net>
2016-08-17 08:14:32 -0700

    AdminUser has_paper_trail

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -3,4 +3,6 @@ class AdminUser < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, 
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_paper_trail
 end
