50458e7d5cd3507f231f66aab0a09fc27e8b9ff2
Jesse B. Hannah <jesse@jbhannah.net>
2016-08-19 21:47:43 -0700

    Add FriendlyId slug to AdminUser

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,8 +1,12 @@
 class AdminUser < ApplicationRecord
+  extend FriendlyId
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  friendly_id :name, use: [:slugged, :history, :reserved]
 
   has_paper_trail
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160820044054) do
+ActiveRecord::Schema.define(version: 20160820044323) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,9 +44,11 @@ ActiveRecord::Schema.define(version: 20160820044054) do
     t.datetime "updated_at",                          null: false
     t.boolean  "superuser"
     t.string   "name"
+    t.string   "slug"
     t.index ["email"], name: "index_admin_users_on_email", unique: true, using: :btree
     t.index ["name"], name: "index_admin_users_on_name", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true, using: :btree
+    t.index ["slug"], name: "index_admin_users_on_slug", unique: true, using: :btree
   end
 
   create_table "friendly_id_slugs", force: :cascade do |t|
