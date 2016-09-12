89f4ae9597ddc461314f4b8c284db7def2835499
Jesse B. Hannah <jesse@jbhannah.net>
2016-09-12 07:24:05 -0700

    Article::PostsController

---
+++ app/controllers/article/posts_controller.rb
@@ -0,0 +1,7 @@
+class Article::PostsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,10 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  namespace :article, path: 'articles' do
+    root 'posts#index'
+    resources :posts, only: :show
+  end
+
   root 'welcome#index'
 end
