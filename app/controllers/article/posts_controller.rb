83399e993b84bde63c1ab013087818b6964cee80
Jesse B. Hannah <jesse@jbhannah.net>
2016-09-12 08:31:07 -0700

    Render article posts

--- app/controllers/article/posts_controller.rb
+++ app/controllers/article/posts_controller.rb
@@ -1,7 +1,9 @@
 class Article::PostsController < ApplicationController
   def index
+    @posts = Article::Post.published
   end
 
   def show
+    @post = Article::Post.published.friendly.find params[:id]
   end
 end
