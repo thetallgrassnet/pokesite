80f094b4f02cbc0f2c41719439b8579a6f577781
Jesse B. Hannah <jesse@jbhannah.net>
2016-12-17 17:16:15 -0700

    Show thumbnail for featured image on index

--- app/decorators/article/post_decorator.rb
+++ app/decorators/article/post_decorator.rb
@@ -1,5 +1,15 @@
 module Article::PostDecorator
   def content
-    action_name == 'show' ? body : excerpt
+    show? ? body : excerpt
+  end
+
+  def display_featured_image
+    show? ? featured_image : featured_image.thumb
+  end
+
+  private
+
+  def show?
+    action_name == 'show'
   end
 end
--- spec/decorators/article/post_decorator_spec.rb
+++ spec/decorators/article/post_decorator_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 describe Article::PostDecorator do
+  include CarrierWave::Test::Matchers
+
   let(:post) { FactoryGirl.build(:article_post).extend Article::PostDecorator }
   subject    { post }
   it         { is_expected.to be_an(Article::Post) }
@@ -18,6 +20,25 @@ describe Article::PostDecorator do
       before { post.expects(:action_name).returns('index') }
 
       it { is_expected.to be_a(SirTrevorRails::Blocks::TextBlock) }
+    end
+  end
+
+  describe '.display_featured_image' do
+    before { Article::Post::FeaturedImageUploader.enable_processing = true }
+    after  { Article::Post::FeaturedImageUploader.enable_processing = false }
+
+    subject { post.display_featured_image }
+
+    context 'show' do
+      before { post.expects(:action_name).returns('show') }
+
+      it { is_expected.to have_dimensions(1280, 720) }
+    end
+
+    context 'index' do
+      before { post.expects(:action_name).returns('index') }
+
+      it { is_expected.to have_dimensions(100, 56) }
     end
   end
 end
