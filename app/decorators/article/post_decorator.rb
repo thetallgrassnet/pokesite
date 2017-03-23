e33d336fbdd75601a09cd5e9890e91858fcb0749
Jesse B. Hannah <jesse@jbhannah.net>
2017-03-23 00:08:08 -0700

    Add column links to byline and post sidebar

--- app/decorators/article/post_decorator.rb
+++ app/decorators/article/post_decorator.rb
@@ -20,14 +20,14 @@ module Article::PostDecorator
   end
 
   def full_byline
-    raw("Posted by #{author.name} on #{time_tag published_at, pubdate: true}")
+    raw("Posted by #{author.name} in #{link_to column.name, column} on #{time_tag published_at, published_at.strftime('%A, %B %-d, %Y at %I:%M %p'), pubdate: true}")
   end
 
   def responsive_byline
     capture do
       concat content_tag(:span, full_byline, class: 'show-for-medium')
       concat(content_tag(:span, class: 'hide-for-medium', 'aria-label' => strip_tags(full_byline)) do
-        raw("#{author.name} — #{time_tag published_at, published_at.strftime('%b %-d, %Y %I:%M %p'), pubdate: true}")
+        raw("#{link_to column.name, column} — #{time_tag published_at, published_at.strftime('%b %-d \'%y %I:%M %p'), pubdate: true}")
       end)
     end
   end
