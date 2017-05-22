8602c2ae42fe2a7088aa0c69b057645ee48e1a8a
Jesse B. Hannah <jesse@jbhannah.net>
2017-05-22 07:55:17 -0700

    Style list of latest non-featured articles

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
     @featured_posts = Article::Post.featured
-    @non_featured_posts = Article::Post.published.where.not(id: @featured_posts.pluck(:id)).limit(10)
+    @non_featured_posts = Article::Post.published.where.not(id: @featured_posts.pluck(:id)).limit(8)
   end
 end
