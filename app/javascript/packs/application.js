f840981be8b3a40e01520664f2dfba8edd077126
Jesse B. Hannah <jesse@jbhannah.net>
2017-06-05 21:58:35 -0700

    Convert time tags to client local time

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,4 +7,12 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-console.log('Hello World from Webpacker')
+const moment = require('moment');
+
+$(document).on('turbolinks:load', function () {
+  document.querySelectorAll('time').forEach(function (el, i, listObj) {
+    console.log(el.textContent);
+    let time = el.getAttribute('datetime');
+    el.textContent = moment(time).format(el.getAttribute('data-format'));
+  });
+});
