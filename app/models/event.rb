24ea3f49f382c6e8b8ce7a64351a5f155812f46e
Jesse B. Hannah <jesse@jbhannah.net>
2017-06-24 21:22:12 -0700

    Implement Event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,22 @@
 class Event < ApplicationRecord
+  extend FriendlyId
+
+  enum event_type: [:competition, :distribution]
+
+  friendly_id :name, use: [:slugged, :history, :reserved]
+
+  has_paper_trail
+
+  mount_uploader :featured_image, FeaturedImageUploader
+
+  scope :upcoming, -> { where('start_date > ?', Date.today) }
+  scope :active,   -> { where('start_date <= ? and end_date >= ?', Date.today, Date.today) }
+  scope :ended,    -> { where('end_date < ?', Date.today) }
+
+  validates :name, presence: true, uniqueness: true
+  validates :featured_image, presence: true
+
+  def to_s
+    name
+  end
 end
