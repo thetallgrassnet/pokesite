7a6dff93b202f4a166a05a5a8e7713cf822fdb2b
Jesse B. Hannah <jesse@jbhannah.net>
2017-06-24 21:22:17 -0700

    Test Event model

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :event do
     name "MyString"
-    featured_image "MyString"
+    featured_image { Rack::Test::UploadedFile.new(Rails.root.join('spec', 'support', 'assets', 'featured_image.png'), 'image/png') }
     event_type 1
     start_date "2017-06-23"
     end_date "2017-06-23"
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,50 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:event) { FactoryGirl.build(:event) }
+  subject { event }
+
+  describe '.upcoming' do
+    before do
+      event.save
+      travel_to 1.day.before(event.start_date)
+    end
+
+    after { travel_back }
+
+    subject { Event.upcoming }
+
+    it { is_expected.to include(event) }
+  end
+
+  describe '.active' do
+    before do
+      event.save
+      travel_to event.start_date
+    end
+
+    after { travel_back }
+
+    subject { Event.active }
+
+    it { is_expected.to include(event) }
+  end
+
+  describe '.ended' do
+    before do
+      event.save
+      travel_to 1.day.after(event.end_date)
+    end
+
+    after { travel_back }
+
+    subject { Event.ended }
+
+    it { is_expected.to include(event) }
+  end
+
+  describe '#to_s' do
+    subject { event.to_s }
+    it      { is_expected.to eql(event.name) }
+  end
 end
